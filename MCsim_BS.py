8db6c009ba2b112d68bd88dc84295aa9c6da7e96
Jonaddo <jonathan.adote@gmail.com>
2018-02-16 10:13:47 +0100

    confidence interval corrected

--- MCsim_BS.py
+++ MCsim_BS.py
@@ -1,14 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
-import pandas as pd
 style.use('ggplot')
 
 M = 80    # Number of simulations
 N = 10    # Number of days to simulate
 S_t = np.zeros([N, M])
-l_bound, u_bound = np.zeros([N, 1]), np.zeros([N, 1])
-S_t[0, :], l_bound[0, 0], u_bound[0, 0] = 17, 17, 17
+S_t[0, :] = 17
 sigma = 0.37
 mu = 0.017
 dt = 1/10
@@ -19,16 +17,14 @@ for i in range(N-1):
         S_t[i+1, j] = S_t[i, j] + mu*S_t[i, j]*dt + sigma*S_t[i, j]*np.sqrt(dt)*Z_t[i, j]
 
 drift = np.mean(S_t, axis=1)
-for i in range(N - 1):
-        l_bound[i+1, 0] = drift[i+1] - 1.96*sigma*np.sqrt(i+1)/np.sqrt(N)
-        u_bound[i+1, 0] = drift[i+1] + 1.96*sigma*np.sqrt(i+1)/np.sqrt(N)
+lower, upper = np.percentile(S_t, [2.5, 97.5], axis=1)
 
 plt.figure(num=1)
 plt.plot(S_t, '#808080')
 plt.plot(drift, '-r', linewidth=2.0)
-plt.plot(l_bound, '-k', linewidth=2.0)
-plt.plot(u_bound, '-k', linewidth=2.0)
+plt.plot(lower, '-k', linewidth=2.0)
+plt.plot(upper, '-k', linewidth=2.0)
 plt.xlabel('time')
 plt.ylabel('Stock price')
-plt.title('Monte Carlo simulation - Euler Scheme')
+plt.title('MC simulation - Black-Scholes model')
 plt.show()
--- MCsim_Heston.py
+++ MCsim_Heston.py
@@ -11,7 +11,7 @@ sigma = 0.37
 mu = 0.017
 kappa = 0.9
 theta = 0.25
-rho = -0.8
+rho = -0.8  # negatively correlated since in bear markets volatility tends to be higher than in bull markets
 S0 = 17
 
 S_t = np.zeros([N, M])
@@ -19,12 +19,6 @@ v_t = np.zeros([N, M])
 Z_t = np.random.normal(0, dt, [N, M])
 Z2 = np.random.normal(0, dt, [N, M])
 Z2_t = rho*Z_t + np.sqrt(1-rho**2)*Z2
-
-# CI interval
-l_bound = np.zeros([N, 1])
-u_bound = np.zeros([N, 1])
-l_bound[0, 0] = S0
-u_bound[0, 0] = S0
 
 S_t[0, :] = S0
 v_t[0, :] = 0.22
@@ -35,15 +29,13 @@ for i in range(N-1):
         v_t[i+1, j] = v_t[i, j] + kappa*(theta - v_t[i, j])*dt + sigma*np.sqrt(v_t[i, j]*dt)*Z2_t[i, j]
 
 drift = np.mean(S_t, axis=1)
-for i in range(N - 1):
-        l_bound[i+1, 0] = drift[i+1] - 1.96*sigma*np.sqrt(i+1)/np.sqrt(N)
-        u_bound[i+1, 0] = drift[i+1] + 1.96*sigma*np.sqrt(i+1)/np.sqrt(N)
-
+lower, upper = np.percentile(S_t, [2.5, 97.5], axis=1)
+lower = np.percentile()
 plt.plot(S_t, '#808080')
 plt.plot(drift, '-r', linewidth=2.0)
-plt.plot(l_bound, '-k', linewidth=2.0)
-plt.plot(u_bound, '-k', linewidth=2.0)
+plt.plot(lower, '-k', linewidth=2.0)
+plt.plot(upper, '-k', linewidth=2.0)
 plt.xlabel('time')
 plt.ylabel('Stock price')
-plt.title('Monte Carlo simulation - Heston model')
+plt.title('MC simulation - Heston model')
 plt.show()
